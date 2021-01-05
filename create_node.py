97fec66c607d0b45f40b3e0aba42b5ef7cd3ba48
Anya <75397581+a-khanzhina@users.noreply.github.com>
2021-01-04 20:33:35 -0500

    Update with  Linked lists

--- create_node.py
+++ create_node.py
@@ -1,26 +1,47 @@
 class Node:
-  def __init__(self, value, link_node=None):
+  def __init__(self, value, next_node=None):
     self.value = value
-    self.link_node = link_node
+    self.next_node = next_node
     
-  def set_link_node(self, link_node):
-    self.link_node = link_node
-    
-  def get_link_node(self):
-    return self.link_node
-  
   def get_value(self):
     return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
 
-## Testing:
-yacko = Node("likes to yak")
-wacko = Node("has a penchant for hoarding snacks")
-dot = Node("enjoys spending time in movie lots")
-
-yacko.set_link_node(dot)
-dot.set_link_node(wacko)
-
-dots_data = yacko.get_link_node().get_value()
-wackos_data = dot.get_link_node().get_value()
-print(dots_data)
-print(wackos_data)
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
