b99675452b98e98ac9294072185422c2e330c1d7
Anya <75397581+a-khanzhina@users.noreply.github.com>
2021-01-09 12:50:05 -0500

    create queue class

--- create_queue.py
+++ create_queue.py
@@ -14,3 +14,36 @@ class Node:
   
   def get_value(self):
     return self.value
+
+ ## QUEUE class:
+
+class Queue:
+  def __init__(self, max_size=None):
+    self.head = None
+    self.tail = None
+    self.max_size = max_size
+    self.size = 0
+    
+  # Add your enqueue method below:
+  
+    
+  def peek(self):
+    if self.is_empty():
+      print("Nothing to see here!")
+    else:
+      return self.head.get_value()
+  
+  def get_size(self):
+    return self.size
+  
+  def has_space(self):
+    if self.max_size == None:
+      return True
+    else:
+      return self.max_size > self.get_size()
+    
+  def is_empty(self):
+    return self.size == 0
+
+q = Queue()
+q.enqueue("all the fluffy kitties")
