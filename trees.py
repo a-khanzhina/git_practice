6e2bbe0a8dd5a765b69ea56992459e361f0f6be5
Anya <75397581+a-khanzhina@users.noreply.github.com>
2021-01-20 12:50:26 -0500

    create class

---
+++ trees.py
@@ -0,0 +1,15 @@
+class TreeNode:
+  def __init__(self, value):
+    self.value = value # data
+    self.children = [] # references to other nodes
+
+  def add_child(self, child_node):
+    # creates parent-child relationship
+    print("Adding " + child_node.value)
+    self.children.append(child_node) 
+    
+  def remove_child(self, child_node):
+    # removes parent-child relationship
+    print("Removing " + child_node.value + " from " + self.value)
+    self.children = [child for child in self.children 
+                     if child is not child_node]
