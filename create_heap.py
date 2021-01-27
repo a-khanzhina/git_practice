f4e6cbc356cfb01d48006f99f5f27ce370150a00
Anya <75397581+a-khanzhina@users.noreply.github.com>
2021-01-27 15:09:13 -0500

    add method "add"

--- create_heap.py
+++ create_heap.py
@@ -2,3 +2,9 @@ class MinHeap:
   def __init__(self):
     self.heap_list = [None]
     self.count = 0
+    
+  def add(self, element):
+    print("Adding {element} to {list}.".format(element = element, list = self.heap_list))
+    self.count +=1
+    self.heap_list.append(element)
+    self.heapify_up()
