5399029673d99987aae5a7698e9b78ff14e88210
Anya <75397581+a-khanzhina@users.noreply.github.com>
2021-01-28 09:58:40 -0500

    add helper methods

--- create_heap.py
+++ create_heap.py
@@ -3,8 +3,22 @@ class MinHeap:
     self.heap_list = [None]
     self.count = 0
     
+  # HEAP HELPER METHODS
+  def parent_idx(self, idx):
+    return idx // 2
+
+  def left_child_idx(self, idx):
+    return idx * 2
+
+  def right_child_idx(self, idx):
+    return idx * 2 + 1
+
+  # END OF HEAP HELPER METHODS  
+  
   def add(self, element):
     print("Adding {element} to {list}.".format(element = element, list = self.heap_list))
     self.count +=1
     self.heap_list.append(element)
     self.heapify_up()
+    
+  
